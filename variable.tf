97ebf836a003a416d402c7e16a37a7f1792ed812
avinashbamne <108449703+avinashbamne@users.noreply.github.com>
2022-07-11 10:28:45 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,7 @@
 variable "region" {
   description = "AWS Deployment region.."
   default = "us-east-1"
+  vpc_cidr             = "10.10.0.0/16"
+  public_subnets_cidr  = "10.10.1.0/24"
+  private_subnets_cidr = "10.10.2.0/24"
 }
